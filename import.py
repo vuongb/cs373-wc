c3fe25ec48278daed2630ada1432b55481cdce06
Boris Jonica <boris.jonica@utexas.edu>
2012-10-23 18:26:19 -0500

    add process function to read and create objects from xml elements

--- import.py
+++ import.py
@@ -8,15 +8,39 @@ SCHEMA='cassie-schema-statistics.xsd'
 #root = tree.getroot()
 #print(root.tag, root.attrib)
 
-def parse_xml(data, schema):
+def get_tree_and_validate(data, schema):
     try:
         wrapper = xsv.parseAndValidateXmlInput(data, schema, xmlIfClass=xsv.XMLIF_ELEMENTTREE)
-        tree = wrapper.getTree()
-        root = tree.getroot()
-               
-
+        validates = wrapper.getTree()
+        return ET.parse(data)
     except xsv.XsvalError as e:
         print("XML did not validate\n"+str(e))
 
+tree = get_tree_and_validate('xml_instances/crisis-breast_cancer.xml', SCHEMA)
 
-parse_xml('crisis-breast_cancer.xml', SCHEMA)
+def process(tree):
+    root = tree.getroot()
+
+    print(str(root.tag))
+    if str(root.tag) == 'world-crises':
+        process(root.getroot())
+    elif str(root.tag) == 'crises':
+        process_crises(root.gettree())
+    elif str(root.tag) == 'organizations':
+        process_organizations(root.gettree())
+    elif str(root.tag) == 'people':
+        process_people(root.gettree())
+
+def process_crises(tree):
+    root = tree.getroot()
+    print(root)
+
+def process_organizations(tree):
+    root = tree.getroot()
+    print(root)
+
+def process_people(tree):
+    root = tree.getroot()
+    print(root)
+
+process(tree)
