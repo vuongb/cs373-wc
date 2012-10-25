80472449949b772326339b1a97433fe94c3d29c4
Boris Jonica <samnosuke@gmail.com>
2012-10-24 19:43:01 -0500

    add exporter unit test

--- test/test_exporter.py
+++ test/test_exporter.py
@@ -1,12 +1,23 @@
+import logging
 import unittest
 from google.appengine.ext import db
 import xml.etree.ElementTree as ETree
-from Models import Crisis
-import exporter
+from Models import *
+from WC1 import get_tree_and_validate
+from exporter import *
 
 class TestExport(unittest.TestCase):
     def setUp(self):
-        pass
+        self.person_obj = Person(us_name='test name', us_type='test type', us_description='test description', us_country='USA')
+        self.crisis_obj = Crisis(us_name='test namec', us_type='test typec', us_description='test descriptionc', us_economicImpact=['$3'], us_humanImpact=['39'], us_resoucesNeeded=['gold'], us_waysToHelp=['donate'])
+        self.org_obj    = Organization(us_name='test nameo', us_type='test typeo', us_description='test descriptiono', us_country='USA', us_email='bob@org.com')
 
-    def test_something(self):
-        pass
+        addPerson(self.person_obj, 0)
+        addCrisis(self.crisis_obj, 0)
+        addOrganization(self.org_obj, 0)
+
+        self.tree = buildTree()
+        self.xml_tree = ETree.tostring(self.tree)
+
+    def test_export(self):
+        assert self.xml_tree
