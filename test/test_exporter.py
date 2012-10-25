a84d7d6b127ad76d8d1431308c4584fb02fa6819
Boris Jonica <samnosuke@gmail.com>
2012-10-24 19:18:36 -0500

    add test_exporter

---
+++ test/test_exporter.py
@@ -0,0 +1,12 @@
+import unittest
+from google.appengine.ext import db
+import xml.etree.ElementTree as ETree
+from Models import Crisis
+import exporter
+
+class TestExport(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
