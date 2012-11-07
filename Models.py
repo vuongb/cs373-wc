3f82b2995520435bf46dd662b27d53370785dea2
Boris Jonica <boris.jonica@utexas.edu>
2012-11-07 02:29:29 -0600

    remove outdated todo

--- Models.py
+++ Models.py
@@ -45,7 +45,6 @@ class Video(db.Model):
     assoc_object = db.ReferenceProperty(None, collection_name='videos', required=True)
 
 class Crisis(db.Model):
-    #todo removed required properties for name, type, description, economicimpact, humanimpact, resourcesneeded, waystohelp
     # Base Data
     us_name            = db.StringProperty(required=True)
     us_alternateNames  = db.StringProperty()
