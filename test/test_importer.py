1b0c04c8cc8e069eb6bdc22595417926cd0f6db2
Boris Jonica <boris.jonica@utexas.edu>
2012-11-06 22:19:59 -0600

    update importer to test validation and importing methods

--- test/test_importer.py
+++ test/test_importer.py
@@ -1,47 +1,73 @@
 import unittest
-from WC1 import get_tree_and_validate, etree_to_dict, store_special_classes
+from WC1 import get_tree_and_validate, etree_to_dict
 import importer
 
 class TestImport(unittest.TestCase):
     def setUp(self):
-        self.crisis_xml = open('test/test_crisis.xml', 'r').read()
+        self.crisis_xml  = open('test/test_crisis.xml', 'r').read()
         self.invalid_xml = open('test/test_invalid.xml', 'r').read()
-#        self.org_xml    = open('test/organization-oxfam.xml', 'r').read()
-#        self.person_xml = open('test/person-bono.xml', 'r').read()
+        self.org_xml     = open('test/test_organization.xml', 'r').read()
+        self.person_xml  = open('test/test_person.xml', 'r').read()
+        self.wc2_xml     = open('WC2.xml', 'r').read()
 
-        self.SCHEMA = 'WC2.xsd'
+        self.SCHEMA      = 'WC2.xsd'
         self.crisis_tree = get_tree_and_validate(self.crisis_xml, open(self.SCHEMA, 'r').read())
-#        self.org_tree = get_tree_and_validate(self.org_xml, open(self.SCHEMA, 'r').read())
-#        self.person_tree = get_tree_and_validate(self.person_xml, open(self.SCHEMA, 'r').read())
+        self.org_tree    = get_tree_and_validate(self.org_xml, open(self.SCHEMA, 'r').read())
+        self.person_tree = get_tree_and_validate(self.person_xml, open(self.SCHEMA, 'r').read())
+        self.wc2_tree    = get_tree_and_validate(self.wc2_xml, open(self.SCHEMA, 'r').read())
 
     ##################################################################################
 
-    def test_validate_valid(self):
-        """valid xml should validate
+    def test_validate_valid_crisis(self):
+        """valid crisis xml should validate
         """
-        assert self.crisis_tree
+        assert self.crisis_tree != 0
+
+    def test_validate_valid_org(self):
+        """valid organization xml should validate
+        """
+        assert self.org_tree != 0
+
+    def test_validate_valid_person(self):
+        """valid organization xml should validate
+        """
+        assert self.person_tree != 0
 
     def test_validate_invalid(self):
         """invalid xml should not validate
         """
         self.assertEqual(get_tree_and_validate(self.invalid_xml, open(self.SCHEMA, 'r').read()), 0)
 
+    def test_validate_wc2(self):
+        """WC2 XML should validate"""
+        assert self.wc2_tree != 0
+
     def test_etree_to_dict(self):
+        """ElementTree should fully convert to dict
         """
-        """
+        person_d = etree_to_dict(self.person_tree.getroot())
+        print(str(person_d))
 
-
-
-    def test_upload_crisis(self):
+    def test_import_crisis(self):
+        """should import a crisis"""
         tree = self.crisis_tree
         root = tree.getroot()
-        for i in root.iter():
-            if i.tag == 'crises':
-                # iterate through all crises
-                d = etree_to_dict(i)
+        assert importer.put_objects(root) == True
 
-                for c in d.get('crises'):
-                    if type(c) != str:
-                        result_dict     = importer.process_crisis(c)
-                        crisis          = result_dict.get('crisis')
-                        crisis.put()
+    def test_import_organization(self):
+        """should import a organization"""
+        tree = self.org_tree
+        root = tree.getroot()
+        assert importer.put_objects(root) == True
+#
+    def test_import_person(self):
+        """should import a person"""
+        tree = self.person_tree
+        root = tree.getroot()
+        assert importer.put_objects(root) == True
+#
+    def test_import_wc2(self):
+        """should import our site schema instance"""
+        tree = self.wc2_tree
+        root = tree.getroot()
+        assert importer.put_objects(root) == True
